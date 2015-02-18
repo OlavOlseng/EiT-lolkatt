b791b5ffae393885c09ef08a283a31a36ab47408
OlavOlseng <olseng.o@gmail.com>
2015-02-18 15:35:46 +0100

    Added sound, as well as a background

---
+++ Assets/Scripts/SoundTriggerSegment.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundTriggerSegment : ParallaxSegment {
+
+	public AudioSource source;
+	private float previousX = 0;
+	private bool fired;
+	
+	override public void progress(float x) 
+	{
+
+		float progress = getProgression (x);
+		if (progress > 0.0f && progress <= 1.0f) 
+		{
+			if (!fired && previousX < startX) {
+				fired = true;
+				source.Play();
+			}
+		}
+		else 
+		{
+			fired = false;
+		}
+		this.previousX = x;	
+	}
+}
