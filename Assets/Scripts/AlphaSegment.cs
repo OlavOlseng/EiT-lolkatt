45c3bd0c268865280cf6b481fd9a2487ae5d24a9
OlavOlseng <olseng.o@gmail.com>
2015-02-18 13:36:06 +0100

    Added alpha blending

---
+++ Assets/Scripts/AlphaSegment.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AlphaSegment : ParallaxSegment {
+
+	public float alphaChange;
+
+	public AlphaSegment() 
+	{
+		this.segmentType = ParallaxSegment.SEGMENT_TYPE_ALPHA;
+	}
+
+	override public void progress(float x) 
+	{
+		float progression = getProgression(x);
+		SpriteRenderer sr = GetComponent<SpriteRenderer> ();
+		Color c = new Color(sr.color.r, sr.color.g, sr.color.b,sr.color.a + progression * alphaChange);
+		sr.color = c;
+	}
+}
