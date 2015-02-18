144e5e0c69fd03a505b3cd3905ad91c9d668b6ae
OlavOlseng <olseng.o@gmail.com>
2015-02-18 09:16:29 +0100

    Forgot some files

---
+++ Assets/Scripts/TranslationSegment.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TranslationSegment : ParallaxSegment {
+
+	public TranslationSegment()
+	{
+		this.segmentType = ParallaxSegment.SEGMENT_TYPE_TRANSLATION;
+	}
+
+	override public System.Object progress(float x) 
+	{
+		float progression = 0.0f;
+
+		if (x > startX + duration) {
+				progression = 1.0f;
+		} else if (startX < x) {
+				progression = (x - startX) / duration;
+		}
+		return Vector3.Lerp (new Vector3 (), translation, progression);
+	}
+}
