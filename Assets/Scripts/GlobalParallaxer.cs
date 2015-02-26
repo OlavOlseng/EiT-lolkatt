03a38ef643ce58f4282328c11ebd20feb1750006
OlavOlseng <olseng.o@gmail.com>
2015-02-26 15:39:38 +0100

    Added shaking parallax

--- Assets/Scripts/GlobalParallaxer.cs
+++ Assets/Scripts/GlobalParallaxer.cs
@@ -2,16 +2,20 @@
 using System.Collections;
 
 public class GlobalParallaxer : MonoBehaviour {
-
-	public float dampening;
+	//TODO Add support for multifinger speed scrolling.
+	public float dampening = 0.02f;
 	public float x = 0;
 
 	float dx;
 	float ddx;
-	
+
+	private bool newTouch = false;
+	private bool touching = false;
+
 
 	public float xMax = 1000;
 	public float dxMax = 100;
+	public float speed = 10.0f;
 
 	// Use this for initialization
 	void Start () {
@@ -20,8 +24,40 @@ public class GlobalParallaxer : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		ddx = Input.GetAxis ("Horizontal");
-		dx += -(dx * dampening) + ddx  * 2;
+		float dampening = this.dampening;
+		ddx = 0;
+		float speedMultiplier = 1.0f;
+		if (Input.touchSupported) {
+			Touch[] touches = Input.touches;
+
+			if(touches.Length > 0) {
+				speedMultiplier = touches.Length;
+				Touch t = touches[0];
+				ddx = -t.deltaPosition.x * t.deltaTime;
+				/*
+				if (newTouch && t.deltaPosition.x == 0.0f) {
+					dx = 0;
+				}
+				*/
+				if(!touching) {
+					newTouch = true;
+				}
+				else if (touching){
+					newTouch = false;
+				}
+				touching = true;
+			}
+			else {
+				touching = false;
+				newTouch = false;
+			}
+		} 
+		else {
+			ddx = Input.GetAxis ("Horizontal");
+		}
+		dx -= (dx * dampening);
+		dx += ddx * speed * speedMultiplier;
+
 
 		if (dx > dxMax) {
 			dx = dxMax;
---
+++ Assets/Scripts/ShakingSegment.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShakingSegment : ParallaxSegment {
+
+	public Vector3 halfAngle;
+	public int periods;
+
+	override public void progress(float x) {
+		float progression = getProgression (x);
+		float scale = Mathf.Sin (periods * progression * Mathf.PI * 2);
+		Vector3 newVec = new Vector3 (halfAngle.x * scale, halfAngle.y * scale, halfAngle.z * scale);
+		print (halfAngle);			
+		print (newVec);			
+		transform.Rotate(newVec);
+	}
+}
